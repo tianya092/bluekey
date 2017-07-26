3e0de04ef8c135c4737eb3b5c74fdc0e662ed71b
tianya092 <598721443@qq.com>
2017-07-26 15:14:01 +0800

    漏的文件

---
+++ src/com/bluekey/User.java
@@ -0,0 +1,37 @@
+package com.bluekey;
+
+public class User {
+
+	private int userId;
+	private int belongDep;
+	private String email;
+	private String remember;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public int getBelongDep() {
+		return belongDep;
+	}
+	public void setBelongDep(int belongDep) {
+		this.belongDep = belongDep;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getRemember() {
+		return remember;
+	}
+	public void setRemember(String remember) {
+		this.remember = remember;
+	}
+	
+	
+	
+}
